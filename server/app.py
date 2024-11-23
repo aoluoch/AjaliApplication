2341f62f3372605f5971f776d9cd939a1e9f5f8e
Amos Oluoch <aoluoch271@gmail.com>
2024-11-23 14:22:41 +0300

    migrations

--- server/app.py
+++ server/app.py
@@ -361,5 +361,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         create_admin_user()
-    port=int(os.environ.get("PORT", 5000)) 
-    app.run(debug=True, port=5000)
+    port=int(os.environ.get("PORT", 5555)) 
+    app.run(debug=True, port=5555)
